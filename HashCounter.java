fba07eace509c1b7e7ca295b0a508d65ea805cf1
Jeong Ji Yeon <97jjy@naver.com>
2019-06-05 09:44:22 +0900

    Update HashCounter.java

--- HashCounter.java
+++ HashCounter.java
@@ -1,6 +1,7 @@
 import java.io.IOException;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
